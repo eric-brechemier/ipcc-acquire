ac5f70b9b009ff813b4695c57abfb39295570b22
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-18 10:05:46 +0100

    Add column 'assessment_reports' to export
    Each value is the concatenated list of assessment reports in which an
    author participated, including only distinct, sorted values. The values
    are separated by a comma character ','.

--- acquire.sql
+++ acquire.sql
@@ -2,7 +2,16 @@ USE 'giec';
 SELECT
   authors.id 'id',
   authors.first_name 'first_name',
-  authors.last_name 'last_name'
+  authors.last_name 'last_name',
+  GROUP_CONCAT(
+    DISTINCT participations.ar
+    ORDER BY participations.ar
+    SEPARATOR ','
+  ) 'assessment_reports'
 FROM
-  authors
+  authors,
+  participations
+WHERE
+  authors.id = participations.author_id
+GROUP BY authors.id
 ;
