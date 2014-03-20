0399415b0ad0239feeb418f751598cc4af32e331
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-20 11:25:28 +0100

    Bug fix: increase maximum size of strings concatenated with GROUP_CONCAT()
    I noticed an empty value in converted data, which I tracked down to
    a limitation in the size of strings concatenated with GROUP_CONCAT().
    The default size is 1024, which is reached with 500-1000 authors with
    identifiers over 1-4 digits + separator.
    
    Reference:
    https://dev.mysql.com/doc/refman/5.0/en/server-system-variables.html#sysvar_group_concat_max_len

--- sql/acquire-working-group-categories.sql
+++ sql/acquire-working-group-categories.sql
@@ -1,4 +1,8 @@
 USE 'giec';
+-- set limit for the size of a string concatenated with GROUP_CONCAT to a
+-- value larger than the number of authors (<10,000) times the size of
+-- an identifier + separator (<10) multiplied by 2 for good measure.
+SET group_concat_max_len=200000;
 SELECT
   working_groups.name 'category',
   CONCAT(
