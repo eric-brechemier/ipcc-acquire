27851349e65b4245a239c9bf806b5a65bc5697e1
Eric Bréchemier <medea@eric.brechemier.name>
2014-03-31 10:42:34 +0200

    Add comments to explain the purpose of each SQL script

--- sql/acquire-working-group-categories.sql
+++ sql/acquire-working-group-categories.sql
@@ -1,3 +1,12 @@
+-- export the sets of authors in each cumulated working group
+
+-- A cumulated working group is the set of authors who contributed
+-- to the same working groups, and no others. With 3 working groups,
+-- there are 7 cumulated working groups corresponding to:
+-- * authors who worked in a single working group: WG1, WG2, WG3
+-- * authors who worked in just two groups: WG1+2, WG1+3, WG2+3
+-- * authors who working in all three groups: WG1+2+3
+
 USE 'giec';
 -- set limit for the size of a string concatenated with GROUP_CONCAT to a
 -- value larger than the number of authors (<10,000) times the size of
