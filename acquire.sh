9d0e5c506d2ff5098e9d8ab53ad9eabb16ab6301
Eric Bréchemier <medea@eric.brechemier.name>
2014-04-10 10:21:44 +0200

    Quote strings with ' instead of " when no parameter expansion is expected

--- acquire.sh
+++ acquire.sh
@@ -13,7 +13,7 @@ user=${1:-'root'}
 host=${2:-'localhost'}
 password=${3:-'no password'}
 
-if [ "$password" = "no password" ]
+if [ "$password" = 'no password' ]
 then
   passwordParam=''
 else
@@ -28,85 +28,85 @@ cd $(dirname $0)
 # create output directory
 mkdir -p output
 
-echo "Acquire Author Records"
+echo 'Acquire Author Records'
 $query \
   < sql/acquire-author-records.sql \
   > output/authors.tsv
 
-echo "Acquire Contribution Type Categories"
+echo 'Acquire Contribution Type Categories'
 $query \
   < sql/acquire-contribution-type-categories.sql \
   > output/contribution-type-categories.tsv
 
-echo "Acquire Total Contributions Categories"
+echo 'Acquire Total Contributions Categories'
 $query \
   < sql/acquire-total-contributions-categories.sql \
   > output/total-contributions-categories.tsv
 
-echo "Acquire Role Categories"
+echo 'Acquire Role Categories'
 $query \
   < sql/acquire-role-categories.sql \
   > output/role-categories.tsv
 
-echo "Acquire Working Group Categories"
+echo 'Acquire Working Group Categories'
 $query \
   < sql/acquire-working-group-categories.sql \
   > output/working-group-categories.tsv
 
-echo "Acquire Assessment Report Categories"
+echo 'Acquire Assessment Report Categories'
 $query \
   < sql/acquire-assessment-report-categories.sql \
   > output/assessment-report-categories.tsv
 
-echo "Acquire Institution Categories"
+echo 'Acquire Institution Categories'
 $query \
   < sql/acquire-institution-categories.sql \
   > output/institution-categories.tsv
 
-echo "Acquire Country Categories"
+echo 'Acquire Country Categories'
 $query \
   < sql/acquire-country-categories.sql \
   > output/country-categories.tsv
 
-echo "Acquire Cumulated Working Group Categories"
+echo 'Acquire Cumulated Working Group Categories'
 $query \
   < sql/acquire-cumulated-working-group-categories.sql \
   > output/cumulated-working-group-categories.tsv
 
-echo "Acquire Cumulated Working Group in Assessment Report Categories"
+echo 'Acquire Cumulated Working Group in Assessment Report Categories'
 $query \
   < sql/acquire-cumulated-working-group-in-assessment-report-categories.sql \
   > output/cumulated-working-group-in-assessment-report-categories.tsv
 
-echo "Acquire Chapter in Working Group in Assessment Report Categories"
+echo 'Acquire Chapter in Working Group in Assessment Report Categories'
 $query \
   < sql/acquire-chapter-in-working-group-in-assessment-report-categories.sql \
   > output/chapter-in-working-group-in-assessment-report-categories.tsv
 
-echo "Acquire Country and Working Group categories"
+echo 'Acquire Country and Working Group categories'
 $query \
   < sql/acquire-country-and-working-group-categories.sql \
   > output/country-and-working-group-categories.tsv
 
-echo "Acquire Role in each Working Group categories"
+echo 'Acquire Role in each Working Group categories'
 $query \
   < sql/acquire-role-in-working-group-categories.sql \
   > output/role-in-working-group-categories.tsv
 
-echo "Discipline in each Working Group categories"
+echo 'Discipline in each Working Group categories'
 $query \
   < sql/acquire-discipline-in-working-group-categories.sql \
   > output/discipline-in-working-group-categories.tsv
 
-echo "Type of Institution in each Working Group categories"
+echo 'Type of Institution in each Working Group categories'
 $query \
   < sql/acquire-institution-type-in-working-group-categories.sql \
   > output/institution-type-in-working-group-categories.tsv
 
-echo "Acquire Years of Assessment Reports"
+echo 'Acquire Years of Assessment Reports'
 $query \
   < sql/acquire-assessment-report-years.sql \
   > output/assessment-report-years.tsv
 
 ls -l -h output
-echo "Export Complete"
+echo 'Export Complete'
