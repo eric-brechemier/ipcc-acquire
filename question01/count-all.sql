1cccb17e853b32e98726adc8f8dacd0327507998
Eric Bréchemier <medea@eric.brechemier.name>
2014-06-19 16:59:57 +0200

    Count authors for each distinct value of total AR participations

---
+++ question01/count-all.sql
@@ -0,0 +1,31 @@
+-- AUTHORS WITH MULTIPLE PARTICIPATIONS
+-- Q1. Who are the authors that have participated
+--     in more than 1, 2, 3, or 4 assessment reports?
+
+USE giec
+
+-- Count the authors who contributed to:
+-- * 1+ assessment reports
+-- * 2+ assessment reports
+-- * 3+ assessment reports
+-- * 4+ assessment reports
+-- * 5  assessment reports
+
+SELECT
+  total_ar_participations,
+  COUNT(author_id) AS total_authors
+FROM
+(
+  SELECT
+    author_id,
+    COUNT(ar) AS total_ar_participations
+  FROM
+  (
+    SELECT author_id, ar
+    FROM participations
+    GROUP BY author_id, ar
+  ) distinct_ar_participations
+  GROUP BY author_id
+) ar_participations
+GROUP BY total_ar_participations
+;
